b713846cd69fa31b42fca7fefa29858bb806bfcd
Burrows2775 <matteolucas112@gmail.com>
2025-04-03 19:02:57 +0200

    Sécurité améliorée et réponses après l'envoi de la commande (Erreurs potentielles)

--- lua/autorun/sh_slaycmd.lua
+++ lua/autorun/sh_slaycmd.lua
@@ -19,21 +19,34 @@ end
 if CLIENT then
 
     hook.Add("OnPlayerChat", "MessageChat", function(ply, text)
-        if string.StartsWith(text, "!slay ") then
-            local table command = string.Explode(" ", text, false)
-            local table allPlayers = player.GetAll()
-            for k, v, i in ipairs(allPlayers) do
-                if v:Nick() == command[2] then
+        if string.StartsWith(text, "!slay ") and LocalPlayer():IsAdmin() then
+            local command = string.Explode(" ", text, false)
+            local target = command[2]
+            
+            local allPlayers = player.GetAll();
+            local correspond = 0;
+
+            for k,v in ipairs(allPlayers) do
+                if v:Nick() == target then
                     net.Start("CmdSlay")
                     net.WriteEntity(v)
                     net.SendToServer()
+                    correspond = correspond + 1
                 end
             end
-        end
+
+            if correspond <= 0 then
+                ply:PrintMessage(HUD_PRINTTALK, "Joueur \"" .. target .. "\" introuvable")
+            end
+
+        elseif !LocalPlayer():IsAdmin() then
+            ply:PrintMessage(HUD_PRINTTALK, "Vous n'êtes pas admin !")
+        end            
+    return ""
     end)
     
     net.Receive("CmdSlaySound", function()
         surface.PlaySound("spongebob.wav")
-    end)z
+    end)
 
 end
